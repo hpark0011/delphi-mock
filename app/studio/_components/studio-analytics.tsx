352368f55f058c27b6f9c7f475d286c1eb8d410b
Hyunsol Park <hpark0011@gmail.com>
2026-01-05 14:10:14 +0900

    add divider dark mode

--- app/studio/_components/studio-analytics.tsx
+++ app/studio/_components/studio-analytics.tsx
@@ -87,7 +87,7 @@ export function StudioAnalytics({ engagements }: { engagements: Engagements }) {
               isPositive={metric.isPositive}
             />
             {index < metrics.length - 1 && (
-              <div className='h-[1px] bg-sand-5 dark:bg-[#171715]' />
+              <div className='h-[1px] min-h-[1px] bg-sand-5 dark:bg-sand-2' />
             )}
           </Fragment>
         ))}
