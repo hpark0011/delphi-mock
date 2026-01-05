47ea203e6235437064e4df15daf8d246c28c639f
Hyunsol Park <hpark0011@gmail.com>
2026-01-06 06:43:35 +0900

    refine verification step

--- app/onboarding/_components/onboarding-steps/start-verification-step.tsx
+++ app/onboarding/_components/onboarding-steps/start-verification-step.tsx
@@ -20,54 +20,53 @@ export function StartVerificationStep() {
   };
 
   return (
-    <div className='flex flex-col items-center justify-center h-full relative w-full'>
-      <div className='flex flex-col items-center justify-center gap-8 w-full'>
+    <div className='flex flex-col items-center justify-center h-full relative w-full mb-20'>
+      <div className='flex flex-col items-center justify-center gap-12 w-full'>
         {/* Heading and description */}
         <div className='flex flex-col gap-4 items-center justify-center max-w-md w-full'>
-          <h1 className='text-3xl font-medium'>Let&apos;s get to know you</h1>
-          <p className='text-text-muted font-[480] text-center leading-[140%] text-[15px]'>
+          <h1 className='text-4xl font-medium'>Let&apos;s get to know you</h1>
+          <p className='text-text-muted font-[480] text-center leading-[140%]'>
             You can only make a Delphi of yourself.
           </p>
         </div>
 
-        {/* LinkedIn Handle Input */}
-        <div className='flex flex-col gap-2 w-full max-w-md '>
-          <Label htmlFor='linkedin-handle'>LinkedIn Handle</Label>
-          <Input
-            id='linkedin-handle'
-            type='text'
-            defaultValue='linkedin.com/in/hyunsolpark'
-            className='w-full'
-          />
+        <div className='flex flex-col gap-0 items-center justify-center max-w-md w-full'>
+          {/* LinkedIn Handle Input */}
+          <div className='flex flex-col gap-2.5 w-full'>
+            <div className='flex items-center justify-start w-full px-1'>
+              <Label htmlFor='linkedin-handle'>LinkedIn Handle</Label>
+            </div>
+            <Input
+              id='linkedin-handle'
+              type='text'
+              defaultValue='linkedin.com/in/hyunsolpark'
+              className='w-full rounded-lg h-10'
+            />
+          </div>
+
+          {/* Action Buttons */}
+          <div className='flex gap-4 items-center justify-center flex-col w-full mt-4'>
+            <Button
+              size='lg'
+              className='w-full rounded-full h-11'
+              variant='primary'
+              onClick={handleVerifyLinkedIn}
+            >
+              Verify Using LinkedIn
+            </Button>
+            <Button
+              size='sm'
+              variant='ghost'
+              className='hover:bg-transparent text-text-muted text-sm'
+            >
+              Verify Using Government ID
+            </Button>
+          </div>
         </div>
 
-        {/* Action Buttons */}
-        <div className='flex gap-2 items-center justify-center flex-col w-full mt-4'>
-          <Button
-            size='lg'
-            className='w-full rounded-full max-w-[348px]'
-            variant='primary'
-            onClick={handleVerifyLinkedIn}
-          >
-            Verify Using LinkedIn
-          </Button>
-          <Button
-            size='sm'
-            variant='ghost'
-            className='hover:bg-transparent text-text-muted'
-          >
-            Verify Using Government ID
-          </Button>
-        </div>
-
-        <div className='mt-8 flex items-center justify-center'>
+        <div className='absolute bottom-0'>
           <OnboardingPrivacyStatement />
         </div>
-      </div>
-
-      {/* Stylized "N" in bottom-right */}
-      <div className='absolute bottom-4 right-4'>
-        <span className='text-text-muted text-2xl font-medium'>N</span>
       </div>
     </div>
   );
